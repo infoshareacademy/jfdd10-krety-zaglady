634e1ae97a0dc4cea8b3f62f3c1b6f2fbc0c1f6c
Balbuhhha <balbuhhha@gmail.com>
2018-09-13 16:24:41 +0200

    images change when scrolling up

--- team-parallax.js
+++ team-parallax.js
@@ -1,22 +1,22 @@
 
 // Gdy scroll będzie na pozycji "#team" nadaj klasę "active" zdjęciom w sekcji team
 
-//1. znajdź zdjęcia i pozycję #team
+//1. znajdź zdjęcia
 var images = document.querySelectorAll('.team-member .picture');
 
-//2. Jeśli jesteśmy 
-window.addEventListener('scroll', function(){
+//2. Gdy jesteśmy na pozycji
+window.addEventListener('scroll', function () {
     var screenOffset = window.scrollY;
     var target = document.querySelector('#team')
 
-    if (screenOffset > target.offsetTop -window.innerHeight/2){
-        images.forEach(function(image){
-            image.classList.add('true')   
+    if (screenOffset > target.offsetTop - window.innerHeight / 1.5) {
+        images.forEach(function (image) {
+            image.classList.add('true')
         })
-        //images[0].classList.add('Ola-true');
-        //images[1].classList.add('Balbina-true');
-        //images[2].classList.add('Lukasz-true');
-        //images[3].classList.add('Monika-true');
-    } 
-    
+    } else {
+        images.forEach(function (image) {
+            image.classList.remove('true')
+        })
+    }
+
 })
