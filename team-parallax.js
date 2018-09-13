301d9c7f49535a2ff1ebe01d2c209cf2a3cdf3a4
Balbuhhha <balbuhhha@gmail.com>
2018-09-13 16:13:14 +0200

    Bartosz naprawil team-parallax.js

--- team-parallax.js
+++ team-parallax.js
@@ -2,15 +2,21 @@
 // Gdy scroll będzie na pozycji "#team" nadaj klasę "active" zdjęciom w sekcji team
 
 //1. znajdź zdjęcia i pozycję #team
-var image = document.querySelectorAll('.team-member .picture');
-//var teamSection = document.querySelector('#team');
+var images = document.querySelectorAll('.team-member .picture');
 
-
+//2. Jeśli jesteśmy 
 window.addEventListener('scroll', function(){
     var screenOffset = window.scrollY;
-    if (screenOffset > 886){
-        image[0].classList.add('Ola-true');
+    var target = document.querySelector('#team')
+
+    if (screenOffset > target.offsetTop -window.innerHeight/2){
+        images.forEach(function(image){
+            image.classList.add('true')   
+        })
+        //images[0].classList.add('Ola-true');
+        //images[1].classList.add('Balbina-true');
+        //images[2].classList.add('Lukasz-true');
+        //images[3].classList.add('Monika-true');
     } 
     
 })
-//2. 
