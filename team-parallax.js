6ce9ccce954acdca3b9b9448e906f1d7f9cd40ed
Balbuhhha <balbuhhha@gmail.com>
2018-09-13 15:26:42 +0200

    linked .js to html

---
+++ team-parallax.js
@@ -0,0 +1,6 @@
+
+// Gdy scroll będzie na pozycji "#team" nadaj klasę "active" zdjęciom w sekcji team
+
+//1. znajdź zdjęcia i pozycję #team
+var image = document.querySelectorAll('.team-member a:first-of-type')
+//2. 
