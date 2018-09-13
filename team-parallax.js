0520da3626471b9f447bce68f16005ad9e3c73e2
Balbuhhha <balbuhhha@gmail.com>
2018-09-13 16:02:01 +0200

    changed picture on scroll

--- team-parallax.js
+++ team-parallax.js
@@ -2,5 +2,15 @@
 // Gdy scroll będzie na pozycji "#team" nadaj klasę "active" zdjęciom w sekcji team
 
 //1. znajdź zdjęcia i pozycję #team
-var image = document.querySelectorAll('.team-member a:first-of-type')
+var image = document.querySelectorAll('.team-member .picture');
+//var teamSection = document.querySelector('#team');
+
+
+window.addEventListener('scroll', function(){
+    var screenOffset = window.scrollY;
+    if (screenOffset > 886){
+        image[0].classList.add('Ola-true');
+    } 
+    
+})
 //2. 
