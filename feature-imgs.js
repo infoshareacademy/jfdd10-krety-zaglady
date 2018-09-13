206b216647ad96ec3bab5b3c7abb674cccbba598
Balbuhhha <balbuhhha@gmail.com>
2018-09-13 22:17:49 +0200

    changed 0.5 tp -0.6

--- feature-imgs.js
+++ feature-imgs.js
@@ -2,7 +2,7 @@ var imageKret = document.querySelector('.kret');
 
 window.addEventListener('scroll', function () {
     var screenOffset = window.scrollY;
-    var kretOffset = screenOffset * 0.5;
+    var kretOffset = screenOffset * 0.6;
 
     imageKret.style.marginRight = (1200 - kretOffset) + 'px';
 })
