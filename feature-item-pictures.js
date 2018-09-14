c5da49942d8d4758adccbf5711f1cf283a6da719
Balbuhhha <balbuhhha@gmail.com>
2018-09-15 00:25:54 +0200

    increased feature divs and added text; no particular idea what to do next

--- feature-item-pictures.js
+++ feature-item-pictures.js
@@ -1,18 +1,21 @@
-var featureImages = document.querySelectorAll('.feature-item-picture');
-var headingsBottom = document.querySelectorAll('.feature-item-text');
-var headingsTop = document.querySelectorAll('.feature-item-picture h2');
-var featureTexts = document.querySelectorAll('.feature-item-picture p');
+//var featureImages = document.querySelectorAll('.feature-item-picture');
+//var headingsBottom = document.querySelectorAll('.feature-item-text');
+//var headingsTop = document.querySelectorAll('.feature-item-picture h2');
+//var featureTexts = document.querySelectorAll('.feature-item-picture p');
 
-var features = [featureImages, headingsBottom, headingsTop, featureTexts]
-var i
+// var features = [featureImages, headingsBottom, headingsTop, featureTexts]
+// var i
 
-featureImages[0].addEventListener('click', function () {
-    featureImages[0].classList.add('active');
-    headingsBottom[0].classList.add('active');
-    headingsTop[0].classList.add('active');
-    featureTexts[0].classList.add('active');
-})
-    //for (var i = 0; i < 4; i += 1){
+// featureImages[0].addEventListener('click', function () {
+//     featureImages[0].classList.add('active');
+//     headingsBottom[0].classList.add('active');
+//     headingsTop[0].classList.add('active');
+//     featureTexts[0].classList.add('active');
+// })
+//     
+
+//pierwsze proby:
+//for (var i = 0; i < 4; i += 1){
     //    features.forEach(function(feature){
     //    feature[i].classList.add('active')})
     //}
