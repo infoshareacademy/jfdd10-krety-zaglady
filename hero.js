63b4e682f1b06894e62c7c121f6e6fc46ed7e994
Lukasz <rgpszczecin@gmail.com>
2018-09-14 16:52:41 +0200

    Animation Hero

--- hero.js
+++ hero.js
@@ -4,8 +4,8 @@ var deviceNode = document.querySelector('.hero-tlo1');
 window.addEventListener('scroll', function () {
   var screenOffset = window.scrollY;
   var deviceOffset = screenOffset * 0.5;
-  deviceNode.style.top = (30 + deviceOffset) + 'px';
-  deviceNode.style.left = (100 - deviceOffset) + 'px';
+  deviceNode.style.top = (0 + deviceOffset) + 'px';
+  deviceNode.style.left = (300 - deviceOffset) + 'px';
 })
 //clouds
 var deviceNode2 = document.querySelector('.hero-tlo2');
