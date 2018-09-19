9992e0854064bdfdd1267876d06103adfff3f3b1
Lukasz <rgpszczecin@gmail.com>
2018-09-19 16:03:04 +0200

    Cleaning in code3

---
+++ game/js/game.js
@@ -0,0 +1,29 @@
+var position = 430;
+var velocity = 0;
+var player = document.querySelector('.basket');
+var goLeft = false;
+var goRight = false;
+var dTime = 16;
+var keyPressed = false;
+window.addEventListener('keydown', function (event) {
+    keyPressed = event.code;
+})
+window.addEventListener('keyup', function (event) {
+    keyPressed = false;
+});
+setInterval(function () {
+    if (keyPressed) {
+        if (keyPressed === 'KeyZ') {
+            velocity = -1;
+        } else if (keyPressed === 'KeyM') {
+            velocity = 1;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+    console.log(parseInt(position), parseInt(velocity));
+    position = Math.min(Math.max(0, position + velocity), 940 - 80);
+    player.style.left = position + 'px';
+}, dTime)
