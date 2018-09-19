971f9792cae61a1e265d1620a214cc6ef6e41f43
Balbuhhha <balbuhhha@gmail.com>
2018-09-19 16:33:53 +0200

    fruit goes down

--- game/js/fruit_movement.js
+++ game/js/fruit_movement.js
@@ -1,8 +1,16 @@
 var levelWindow = document.querySelector('.level_window');
 var fruit = document.createElement('div');
 
-
 levelWindow.appendChild(fruit);
 
 fruit.classList.add('fruit');
+
+var position = 0;
+
+setInterval (function () {
+  position += 1;
+  fruit.style.marginTop = position + 'px';
+}, 16)
+
+
 
