bc7bb8201aaf38511cc1685c5f9dbc60c190bcd6
sq1fya <rgpszczecin@gmail.com>
2018-09-20 23:51:12 +0200

    game_2 random coin colision WORK

--- game_2/js/drop_coin.js
+++ game_2/js/drop_coin.js
@@ -1,10 +1,19 @@
-function moveCoins () {
-    var element = addCoin()
-    var position = 0;
-    setInterval(function () {
-      position += 1;
-      element.style.top = position + 'px';
-    }, 16)
-    // element.style.top = '10px';
+function moveCoins() {
+  var element = addCoin()
+  var position = 0;
+  // var coinLeft = document.querySelector('.coin').offsetLeft;
+  // var triangleHeight = document.querySelector('.triangle').offsetTop;
+  // var triangleLeft = document.querySelector('.triangle').offsetLeft;
+  setInterval(function () {
+    position += 1;
+    element.style.top = position + 'px';
+    if ((document.querySelector('.coin').offsetTop) >= (document.querySelector('.triangle').offsetTop) && (document.querySelector('.coin').offsetLeft) >= (document.querySelector('.triangle').offsetLeft)) {
+      element.style.background = 'transparent';
+      element.remove;
+      return;
+    }
+    console.log(document.querySelector('.coin').offsetTop);
+  }, 16)
+  // element.style.top = '10px';
 }
 moveCoins();
