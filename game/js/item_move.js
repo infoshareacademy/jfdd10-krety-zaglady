366f70efdf1a5105546e6e7d20180dc7a41c6b9b
Lukasz <rgpszczecin@gmail.com>
2018-09-21 10:35:28 +0200

    interwal delete

---
+++ game/js/item_move.js
@@ -0,0 +1,14 @@
+var getMainWindow = window.querySelectorAll("level_window");
+var item = document.createElement('div');
+levwl_window.classList.add('item');
+
+
+
+// getMainWindow.appendChild(item);
+// addEventListener.appendChild(item);
+// var car = document.querySelector('#car');
+// var position = 0;
+// setInterval(function () {
+    // position += 1;
+    // getMainWindow.style.Left = position + 'px';
+// }, 16)
--- game_2/js/drop_coin.js
+++ game_2/js/drop_coin.js
@@ -4,15 +4,16 @@ function moveCoins() {
   // var coinLeft = document.querySelector('.coin').offsetLeft;
   // var triangleHeight = document.querySelector('.triangle').offsetTop;
   // var triangleLeft = document.querySelector('.triangle').offsetLeft;
-  setInterval(function () {
+  var intervalId = setInterval(function () {
     position += 1;
     element.style.top = position + 'px';
+    console.log(document.querySelector('.coin').offsetTop);
     if ((document.querySelector('.coin').offsetTop) >= (document.querySelector('.triangle').offsetTop) && (document.querySelector('.coin').offsetLeft) >= (document.querySelector('.triangle').offsetLeft)) {
       element.style.background = 'transparent';
       element.remove;
+      // clearInterval(intervalId);
       return;
     }
-    console.log(document.querySelector('.coin').offsetTop);
   }, 16)
   // element.style.top = '10px';
 }
