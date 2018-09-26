11a22addd387dd5dcd09f35d02e7830226ffa08b
Monika Jewula <mp.jewula@gmail.com>
2018-09-26 15:49:31 +0200

    start and end screen - WiP

---
+++ game/js/buttons.js
@@ -0,0 +1,7 @@
+function startGame() {
+document.getElementsByClassName('introScreen').style.display = 'none';
+}
+
+function replay() {
+    document.getElementsByClassName('endScreen').style.display = 'none';
+    }
