4e792583df2ae106f96655ff17a1e6006440ce84
Balbuhhha <balbuhhha@gmail.com>
2018-09-27 16:10:36 +0200

    endScreen appears after timeout so far

--- game/js/buttons.js
+++ game/js/buttons.js
@@ -3,13 +3,24 @@ function introScreenAppears() {
     introScreen.style.display = 'block';
 }
 
+function endScreenAppears() {
+    var endScreen = document.querySelector('.endScreen');
+    endScreen.style.display = 'block';
+}
+
 introScreenAppears()
 
 window.addEventListener('click', function (event) {
     
     if (event.target.classList.contains('button')) {
         var introScreen = document.querySelector('.introScreen');
+        var endScreen = document.querySelector('.endScreen');
         introScreen.style.display = 'none';
+        endScreen.style.display = 'none';
         play();
     }
 })
+
+setTimeout(function() {
+    endScreenAppears()
+}, 3000)
